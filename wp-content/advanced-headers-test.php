296f7230440f37705629195760594915770a6d14
sm0k3rG <jhosepmarca.uta@gmail.com>
2025-07-09 10:53:45 -0400

    Primer commit - Proyecto WordPress con Elementor

---
+++ wp-content/advanced-headers-test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* This file is created by Really Simple SSL to test the CSP header length
+* It will not load during regular wordpress execution
+*/
+
+
+
+ echo '<html><head><meta charset="UTF-8"><META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW"></head><body>Really Simple SSL headers test page</body></html>';
